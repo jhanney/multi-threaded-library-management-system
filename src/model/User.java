b15eef0af58b823e36c53e841f362d892c3c35eb
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2025-11-29 22:18:39 +0000

    Create user class with getters, setters and tostring

---
+++ src/model/User.java
@@ -0,0 +1,68 @@
+package model;
+
+public class User {
+
+	// variables
+	private String name;
+	private String studentId;
+	private String email;
+	private String password;
+	private String department;
+	private String role;
+
+	// getters and setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	// to string
+	@Override
+	public String toString() {
+		return "User [name=" + name + " role=" + role + "]";
+	}
+
+}
