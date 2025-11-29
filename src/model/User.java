59dc4b5fbe523646a797b83bba6c5096690dc431
JoshuaHanney <163124064+JoshuaHanney@users.noreply.github.com>
2025-11-29 22:43:02 +0000

    add constructor with fields

--- src/model/User.java
+++ src/model/User.java
@@ -9,6 +9,17 @@ public class User {
 	private String password;
 	private String department;
 	private String role;
+	
+	//constructor with fields 
+	public User(String name, String studentId, String email, String password, String department, String role) {
+		super();
+		this.name = name;
+		this.studentId = studentId;
+		this.email = email;
+		this.password = password;
+		this.department = department;
+		this.role = role;
+	}
 
 	// getters and setters
 	public String getName() {
